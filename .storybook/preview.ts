e13c2289cd53de024bbd7f6dbf92320d0acfe3bc
Cliff Pyles <cliff@cliffpyles.com>
2024-05-17 11:23:45 -0400

    Add support for styled-components in Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,16 @@
 import type { Preview } from "@storybook/react";
-import withThemeProvider from './withThemeProvider';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+
+/* TODO: update import for your custom theme configurations */
+import { lightTheme, darkTheme } from '../src/themes/themes';
+
+/* TODO: replace with your own global styles, or remove */
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+  `;
 
 const preview: Preview = {
   parameters: {
@@ -24,7 +35,15 @@ const preview: Preview = {
       },
     },
   },
-  decorators: [withThemeProvider],
+  decorators: [withThemeFromJSXProvider({
+    themes: {
+      light: lightTheme,
+      dark: darkTheme,
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+    GlobalStyles,
+  })],
   tags: ['autodocs']
 };
 
